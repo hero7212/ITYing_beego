158fd8a1c8a6aac891a56d1ba19c517d24aad092
frontendgod <3054750147@qq.com>
2020-05-17 16:29:15 +0800

    p51

---
+++ controllers/user.go
@@ -0,0 +1,73 @@
+package controllers
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/astaxie/beego"
+)
+
+type UserController struct {
+	beego.Controller
+}
+
+func (c *UserController) Get() {
+
+	c.Ctx.WriteString("用户中心")
+}
+
+func (c *UserController) AddUser() {
+
+	c.TplName = "userAdd.html"
+}
+
+// 处理post请求 获取post提交的数据
+func (c *UserController) DoAddUser() {
+	id, err := c.GetInt("id")
+	if err != nil {
+		c.Ctx.WriteString("id必须是int类型")
+		return
+	}
+	// fmt.Printf("%v---%T", id, id)
+	username := c.GetString("username")
+	password := c.GetString("password")
+	hobby := c.GetString("hobby")
+
+	fmt.Printf("值：%v---类型：%T", hobby, hobby)
+
+	c.Ctx.WriteString("用户中心--" + strconv.Itoa(id) + username + password)
+}
+
+func (c *UserController) EditUser() {
+
+	c.TplName = "userEdit.html"
+}
+
+// 定义一个user的结构体
+type User struct {
+	Username string   `form:"username" json:"username"`
+	Password string   `form:"password" json:"password"`
+	Hobby    []string `form:"hobby" json:"hobby"`
+}
+
+func (c *UserController) DoEditUser() {
+
+	u := User{}
+	err := c.ParseForm(&u)
+	if err := c.ParseForm(&u); err != nil {
+		c.Ctx.WriteString("Post提交失败")
+	}
+	fmt.Printf("%#v", u)
+	c.Ctx.WriteString("解析json数据成功")
+}
+
+func (c *UserController) GetUser() {
+	u := User{
+		Username: "张三",
+		Password: "123456",
+		Hobby:    []string{"1", "2"},
+	}
+	// 返回一个json数据
+	c.Data["json"] = u
+	c.ServeJSON()
+}
--- routers/router.go
+++ routers/router.go
@@ -12,4 +12,11 @@ func init() {
 	beego.Router("/article", &controllers.ArticleController{}) // Get方法
 	beego.Router("/article/add", &controllers.ArticleController{}, "get:AddArticle")
 	beego.Router("/article/edit", &controllers.ArticleController{}, "get:EditArticle")
+
+	beego.Router("/user", &controllers.UserController{})
+	beego.Router("/user/add", &controllers.UserController{}, "get:AddUser")
+	beego.Router("/user/doAdd", &controllers.UserController{}, "post:DoAddUser")
+	beego.Router("/user/edit", &controllers.UserController{}, "get:EditUser")
+	beego.Router("/user/doEdit", &controllers.UserController{}, "post:DoEditUser")
+	beego.Router("/user/getUser", &controllers.UserController{}, "get:GetUser")
 }
