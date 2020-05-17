45eaece0b9584359b25b727efebe707c8b434b7a
frontendgod <3054750147@qq.com>
2020-05-17 18:10:19 +0800

    p52

--- controllers/goods.go
+++ controllers/goods.go
@@ -9,8 +9,15 @@ type GoodsController struct {
 }
 
 func (c *GoodsController) Get() {
-
 	c.Data["title"] = "你好，商品"
 	c.Data["num"] = 12
 	c.TplName = "goods.tpl"
+}
+
+func (c *GoodsController) DoAdd() {
+	c.Ctx.WriteString("执行增加操作")
+}
+
+func (c *GoodsController) DoEdit() {
+	c.Ctx.WriteString("执行增加操作")
 }
